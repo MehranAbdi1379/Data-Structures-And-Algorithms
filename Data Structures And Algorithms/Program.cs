4f322212e43889a8a73bbd2ccd9a4a8977ea2fbe
mehra <mehra@MEHRAN-ABDO>
2023-03-03 08:29:20 +0330

    Add project files.

---
+++ Data Structures And Algorithms/Program.cs
@@ -0,0 +1,58 @@
+﻿using Data_Structures_And_Algorithms;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CArray nums = new CArray(50);
+
+        for (int i = 0; i < 50; i++)
+        {
+            nums.Insert(i);
+        }
+
+        nums.DisplayElements();
+    }
+}
+
+public class CArray
+{
+    public int[] arr { get; set; }
+    public int upper { get; set; }
+    public int numElements { get; set; }
+
+    public CArray(int size)
+    {
+        arr = new int[size];
+        upper = size -1 ;
+        numElements = 0;
+    }
+
+    public void Insert(int item)
+    {
+        arr[numElements] = item;
+        numElements++;
+    }
+
+    public void DisplayElements()
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < upper; i++)
+        {
+            arr[i] = 0;
+        }
+        numElements = 0;
+    }
+}
---
+++ Data Structures And Algorithms/Timing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithms
+{
+    public class Timing
+    {
+        public TimeSpan StartTime;
+        public TimeSpan Duration;
+
+        public Timing()
+        {
+            StartTime = new TimeSpan(0);
+            Duration = new TimeSpan(0);
+        }
+
+        public void StartTimer()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            StartTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+        }
+
+        public void EndTimer()
+        {
+            Duration = Process.GetCurrentProcess().Threads[0].UserProcessorTime.Subtract(StartTime);
+        }
+
+        public double Result()
+        {
+            return Duration.TotalSeconds;
+        }
+    }
+}

0c05d42bf6448d67918296f0dc76d97ebbfc0e35
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-03 09:06:48 +0330

    Creating Bubble and Selection Sort Methods

---
+++ Data Structures And Algorithms/CArray.cs
@@ -0,0 +1,75 @@
+﻿public class CArray
+{
+    public int[] arr { get; set; }
+    public int upper { get; set; }
+    public int numElements { get; set; }
+
+    public CArray(int size)
+    {
+        arr = new int[size];
+        upper = size -1 ;
+        numElements = 0;
+    }
+
+    public void Insert(int item)
+    {
+        arr[numElements] = item;
+        numElements++;
+    }
+
+    public void DisplayElements()
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < upper; i++)
+        {
+            arr[i] = 0;
+        }
+        numElements = 0;
+    }
+
+    public void BubbleSort()
+    {
+        int temp;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length-1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    temp = arr[j+1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void SelectionSort()
+    {
+        for (int i = 0; i < arr.Length -1; i++)
+        {
+            int iMin = i;
+            for (int j = i+1 ; j < arr.Length; j++)
+            {
+                if (arr[iMin] > arr[j])
+                {
+                    iMin = j;
+                }
+            }
+            if (iMin != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[iMin];
+                arr[iMin] = temp;
+            }
+        }
+    }
+}
--- Data Structures And Algorithms/Program.cs
+++ Data Structures And Algorithms/Program.cs
@@ -9,50 +9,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        CArray nums = new CArray(50);
+        CArray nums = new CArray(10);
+        Random random = new Random();
 
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < 10; i++)
         {
-            nums.Insert(i);
+            nums.Insert((int)(random.NextDouble() * 100)); 
         }
 
+        Console.Write("Numbers before sorting: ");
+        nums.DisplayElements();
+
+        nums.BubbleSort();
+        Console.Write("Numbers after Bubble Sorting: ");
         nums.DisplayElements();
     }
 }
 
-public class CArray
-{
-    public int[] arr { get; set; }
-    public int upper { get; set; }
-    public int numElements { get; set; }
-
-    public CArray(int size)
-    {
-        arr = new int[size];
-        upper = size -1 ;
-        numElements = 0;
-    }
-
-    public void Insert(int item)
-    {
-        arr[numElements] = item;
-        numElements++;
-    }
-
-    public void DisplayElements()
-    {
-        foreach (int item in arr)
-        {
-            Console.Write(item + " ");
-        }
-    }
-
-    public void Clear()
-    {
-        for (int i = 0; i < upper; i++)
-        {
-            arr[i] = 0;
-        }
-        numElements = 0;
-    }
-}

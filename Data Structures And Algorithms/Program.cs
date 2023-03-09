5c1ad06ba0562b99b4fd8600b8d87c2e4d545e9e
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-09 08:29:10 +0330

    Practicing Sets

--- Data Structures And Algorithms/Program.cs
+++ Data Structures And Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Data_Structures_And_Algorithms;
+using System;
 using System.Collections;
 using System.Diagnostics;
 using System.Globalization;
@@ -10,221 +11,106 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
-        
-        Random random = new Random();
-        List<int> numbers = new List<int>();
-
-        for (int i = 0; i < 10000; i++)
-        {
-            int number = Convert.ToInt32(random.NextDouble() * 10);
-            if (number == 10)
-            {
-                numbers.Add(number-1);
-            }
-            else
-            {
-                numbers.Add(number);
-            }
-        }
-
-        foreach (int item in numbers)
-        {
-            binarySearchTree.Insert(item);
-        }
-
-        Dictionary<int , int> dictionary = new Dictionary<int, int>();
-
-        for (int i = 0; i < 10; i++)
-        {
-            dictionary.Add(i, 0);
-        }
-
-        foreach (int item in numbers)
-        {
-            int theCounter = dictionary[item];
-            theCounter++;
-            dictionary[item] = theCounter;    
-        }
-
-        foreach (var item in dictionary)
-        {
-            Console.WriteLine(item.Key + ": " + item.Value);
-        }
-
-    }
-}
-
-public class Node
-{
-    public int Data{ get; set; }
-    public Node Left { get; set; }
-    public Node Right { get; set; }
-
-    public void DisplayNode()
-    {
-        Console.Write(Data + " ");
-    }
-}
-
-public class BinarySearchTree
-{
-    public Node root;
-    public int Count = 0;
-
-    public BinarySearchTree()
-    {
-        root = null;
-    }
-
-    public Node Find(int item)
-    {
-        Node current = root;
-
-        while (true)
-        {
-            if (current.Data == item)
-            {
-                return current;
-            }
-            else if (item<current.Data)
-            {
-                if (current.Left == null)
-                {
-                    return null;
-                }
-                else
-                {
-                    current = current.Left;
-                }
-            }
-            else if (item > current.Data)
-            {
-                if (current.Right == null)
-                {
-                    return null;
-                }
-                else
-                {
-                    current = current.Right;
-                }
-            }
-        }
-    }
-
-    public int FindMin()
-    {
-        Node current = root;
-        while (current.Left != null)
-        {
-            current = current.Left;
-        }
-
-        return current.Data;
-    }
-
-    public int FindMax()
-    {
-        Node current = root;
-
-        while (current.Right != null)
-        {
-            current = current.Right;
-        }
-
-        return current.Data;
-    }
-
-    public void InOrder(Node theRoot)
-    {
-        if (root != null)
-        {
-            if (theRoot.Left != null)
-            {
-                InOrder(theRoot.Left);
-            }
-            theRoot.DisplayNode();
-            if (theRoot.Right != null)
-            {
-                InOrder(theRoot.Right);
-            }
-            
-        }
-    }
-
-    public void PreOrder(Node theRoot)
-    {
-        if (root != null)
-        {
-            theRoot.DisplayNode();
-            if (theRoot.Left != null)
-            {
-                PreOrder(theRoot.Left);
-            }
-            
-            if (theRoot.Right != null)
-            {
-                PreOrder(theRoot.Right);
-            }
-
-        }
-    }
-
-    public void PostOrder(Node theRoot)
-    {
-        if (root != null)
-        {
-            if (theRoot.Left != null)
-            {
-                PostOrder(theRoot.Left);
-            }
-            
-            if (theRoot.Right != null)
-            {
-                PostOrder(theRoot.Right);
-            }
-            theRoot.DisplayNode();
-        }
-    }
-
-    public void Insert(int i)
-    {
-        Node newNode = new Node();
-        newNode.Data = i;
-        if (root == null)
-        {
-            root = newNode;
-            Count++;
-        }
+        CSet setA = new CSet();
+        CSet setB = new CSet();
+        setA.Add(1);
+        setA.Add(2);
+        setA.Add(3);
+        setB.Add(2);
+        setB.Add(3);
+        CSet setC = new CSet();
+        setC = setA.Union(setB);
+        Console.WriteLine();
+        Console.WriteLine(setA.ToString());
+        Console.WriteLine(setC.ToString());
+        setC = setA.Intersection(setB);
+        Console.WriteLine(setC.ToString());
+        setC = setA.Difference(setB);
+        Console.WriteLine(setC.ToString());
+        bool flag = setB.IsSubset(setA);
+        if (flag)
+            Console.WriteLine("b is a subset of a");
         else
+            Console.WriteLine("b is not a subset of a");
+    }
+}
+
+public class CSet
+{
+    private BitArray data;
+
+    public CSet()
+    {
+        data = new BitArray(5);
+    }
+
+    public void Add(int item)
+    {
+        data[item] = true;
+    }
+
+    public bool IsMember(int item)
+    {
+        return data[item];
+    }
+
+    public void Remove(int item)
+    {
+        data[item] = false;
+    }
+
+    public CSet Union(CSet aSet)
+    {
+        CSet tempSet = new CSet();
+
+        for (int i = 0; i < data.Length; i++)
         {
-            Node current = root;
-            Node parent;
-            while (true)
+            tempSet.data[i] = data[i] || aSet.data[i];
+        }
+        return tempSet;
+    }
+
+    public CSet Intersection(CSet aSet)
+    {
+        CSet tempSet = new CSet();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            tempSet.data[i] = data[i] && aSet.data[i];
+        }
+        return tempSet;
+    }
+
+    public CSet Difference(CSet aSet)
+    {
+        CSet tempSet = new CSet();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            tempSet.data[i] = data[i] && !aSet.data[i];
+        }
+        return tempSet;
+    }
+
+    public bool IsSubset(CSet aSet)
+    {
+        CSet tempSet = new CSet();
+
+        for (int i = 0; i < aSet.data.Count; i++)
+        {
+            if (!(aSet.data[i] && data[i]))
             {
-                parent = current;
-                if (i < current.Data)
-                {
-                    current = current.Left;
-                    if (current == null)
-                    {
-                        parent.Left = newNode;
-                        Count++;
-                        break;
-                    }
-                    
-                }
-                else
-                {
-                    current = current.Right;
-                    if (current == null)
-                    {
-                        parent.Right = newNode;
-                        Count++;
-                        break;
-                    }
-                }
+                return false;
             }
         }
+        return true;
+    }
+    public override string ToString()
+    {
+        string s = "";
+        for (int i = 0; i <= data.Count - 1; i++)
+            if (data[i])
+                s += i;
+        return s;
     }
 }
